b2d094f1a6f4607da8ee2342f2cbdd157402039b
JonathanKievits <Jonathan.kievits@hotmail.nl>
2016-12-09 15:06:29 +0100

    Game with art, still needs fixing though

--- Assets/Scripts/Player/KnockBack.cs
+++ Assets/Scripts/Player/KnockBack.cs
@@ -7,18 +7,18 @@ public class KnockBack : MonoBehaviour
     private Rigidbody2D rb2d;
     private float kbForce;
     private float dmgKb;
-    private PlayerMovement playerMovement;
+    private EnemyMovement enemyMovement;
 
     void Start()
     {
-        playerMovement = GetComponent<PlayerMovement>();
-        kbForce = 7000f;
+        enemyMovement = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyMovement>();
+        kbForce = 30000f;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        dmgKb = kbForce * playerMovement.direction;
+        dmgKb = kbForce * enemyMovement.direction;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -26,11 +26,10 @@ public class KnockBack : MonoBehaviour
         if (other.tag == "Enemy")
         {
             rb2d.AddForce(new Vector2(0f, 5000f));
-            Debug.Log("Hello");
         }
         if(other.tag == "Danger")
         {
-            rb2d.AddForce(new Vector2(dmgKb,500f));
+            rb2d.AddForce(new Vector2(dmgKb,1500f));
         }
 
     }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,17 +8,15 @@ public class PlayerMovement : MonoBehaviour
     private float jumpHeight;
     public Rigidbody2D rb2d;
     private float movex;
-    [HideInInspector]public float direction;
     [HideInInspector]public bool falling;
     
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        moveSpeed = 10f;
+        moveSpeed = 5f;
         jumpHeight = 7f;
         movex = 0f;
         falling = false;
-        direction = 1f;
     }
 
     void Update()
@@ -32,14 +30,6 @@ public class PlayerMovement : MonoBehaviour
                 rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);
                 falling = true;
             }
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            direction = -1;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            direction = 1;
         }
     }
 
